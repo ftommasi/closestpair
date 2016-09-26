cb55552072ba1c7432fcb83b555567dda7ccce4a
Fausto G. Tommasi <mmasito123@yahoo.com>
2016-09-26 11:35:50 -0500

    Cleanup

--- closestpair.cpp
+++ closestpair.cpp
@@ -8,31 +8,6 @@
 using namespace std;
 
 //comparitor functions
-bool CompX(const Point& a,const Point& b){
-  return compareByX(a,b);
-}
-
-bool CompY(const Point& a,const  Point& b){
-  return compareByY(a,b);
-}
-
-void dump(const vector<Point>& vec){
-  for(unsigned int i=0; i<vec.size(); i++){
-    cout << "(" << vec[i].x << ", " << vec[i].y << ")" << ", ";
-  }
-  cout << endl;
-}
-
-Outcome CompMin(Outcome a, Outcome b){
-  if( a.dsq < b.dsq){
-  
-    return a;
-  }
-  else{
-    return b;
-  }
-}
-
 Outcome brute(const vector<Point>& data) {
     Outcome best(data[0], data[1], distSquared(data[0],data[1]));
     for (unsigned int j=1; j < data.size(); j++) {
@@ -46,25 +21,7 @@ Outcome brute(const vector<Point>& data) {
     return best;
 }
 
-Outcome strip(const vector<Point>& ySorted,const Outcome& toBeat){
-  //strip logic
-  Outcome best = toBeat;
-  for(unsigned int i =0; i < ySorted.size(); i++){
-    for(unsigned int j = i+1; 
-      j< ySorted.size() && j < i+7; //(pow((ySorted[j].y - ySorted[i].y),2.0) < best.dsq);
-      j++){
-        if(distSquared(ySorted[i],ySorted[j]) < best.dsq) {
-          best = 
-            Outcome(ySorted[i],ySorted[j],
-            distSquared(ySorted[i],ySorted[j])
-            );//update best
-        }
-    }
-  }
-  return best;
-}	
-
-Outcome proc(const vector<Point>& xSorted, const vector<Point>& ySorted){
+Outcome recursive_search(const vector<Point>& xSorted, const vector<Point>& ySorted){
    if(xSorted.size() <= 3 ){
       return brute(xSorted);
    }
@@ -84,93 +41,41 @@ Outcome proc(const vector<Point>& xSorted, const vector<Point>& ySorted){
 	      }
       }
     
-      /* 
-     
-     cout << "Midpoint (" << midPoint.x << ", " << midPoint.y << ")" << endl;
-     cout << "Dumping left" << endl;
-     dump(leftHalf);
-     cout << "Done Dumping Left" << endl;
-     
-     cout << "Dumping right" << endl;
-     dump(rightHalf);
-     cout << "Done Dumping Right" << endl;
-
-     cout << "Dumping left" << endl;
-     dump(Yleft);
-     cout << "Done Dumping Left" << endl;
-     
-     cout << "Dumping right" << endl;
-     dump(Yright);
-     cout << "Done Dumping Right" << endl;
-     
-     */
-     
-     auto leftdelta = proc(leftHalf,Yleft);
-     auto rightdelta = proc(rightHalf,Yright);
-     //auto delta = CompMin(proc(leftHalf,Yleft),proc(rightHalf,Yright));
-     Outcome delta = leftdelta;
-
-     if(min(leftdelta.dsq,rightdelta.dsq)==rightdelta.dsq){
-       delta = rightdelta;
-     }
-     else{
-       delta = leftdelta;
-     }
-      
+          
+     auto leftdelta = recursive_search(leftHalf,Yleft);
+     auto rightdelta = recursive_search(rightHalf,Yright);
+     Outcome delta = ((leftdelta.dsq < rightdelta.dsq) ? leftdelta : rightdelta);  
+              
      vector<Point> Ystrip;
       for(unsigned int i=0; i < ySorted.size(); i++){
-              //verify this - what is delta
         if(pow(abs(ySorted[i].x - midPoint.x),2.0) <= delta.dsq){
 	        Ystrip.push_back(ySorted[i]);
 	      }
       }
-/*
-    cout << "Midpoint (" << midPoint.x << ", " << midPoint.y << ")" << endl;
-    cout << "delta.dsq = " << delta.dsq << endl; 
-    cout << "Dumping strip" << endl;
-    dump(Ystrip);
-    cout << "Done Dumping strip" << endl;
-  */   
+     
       for(unsigned int i =0; i< Ystrip.size(); i++){
-        for(unsigned int j = 1; j <  9 ; j++){ //next seven points
-          if( (i+j) > Ystrip.size()-1) break;
+        for(unsigned int j = 1; j <  6 ; j++){ //next five points
+          if( (i+j) > Ystrip.size() -1 ) break; //do not go out of bounds
           if(distSquared(Ystrip[i],Ystrip[i+j]) < delta.dsq){
             delta = Outcome(Ystrip[i],Ystrip[i+j],distSquared(Ystrip[i],Ystrip[i+j])) ;
           }
         }
       }
 
-//----------------------------------------------------------------
-/*
-      for(unsigned int i =0; i < Ystrip.size(); i++){
-      for(unsigned int j = i+1; 
-        j< Ystrip.size() && (pow((Ystrip[j].y - Ystrip[i].y),2.0) < delta.dsq);
-        j++){
-          if(distSquared(Ystrip[i],Ystrip[j]) < delta.dsq) {
-            delta= 
-              Outcome(Ystrip[i],Ystrip[j],
-              distSquared(Ystrip[i],Ystrip[j])
-              );//update best
-          }
-       }
-      }
-*/
       return delta;
     }
 }
 
 Outcome efficient(const vector<Point>& data) {
-   vector<Point> delta;
-   Outcome best(data[0],data[1],distSquared(data[0],data[1]));
    //sortByX
    auto xSorted (data);
-   sort(xSorted.begin(),xSorted.end(),CompX);
+   sort(xSorted.begin(),xSorted.end(),compareByX);
    
    //sortbyY
    auto ySorted (data);
-   sort(ySorted.begin(),ySorted.end(),CompY);
+   sort(ySorted.begin(),ySorted.end(),compareByY);
    
-   best = proc(xSorted,ySorted);
+   Outcome best = recursive_search(xSorted,ySorted);
    return best;
 }
 
